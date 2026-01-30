25e97e5a1520a9a7e9f28d64b7462f4b6b90d967
Veerapong <newkubise@gmail.com>
2026-01-30 17:10:07 +0700

    chore(workspace): remove deprecated devserver service
    - Remove services/devserver (deprecated devserver service)
    
    Clean up deprecated devserver service to simplify workspace structure

--- services/devserver/src/config/markdown.config.ts
+++
@@ -1,6 +0,0 @@
-import type { HighlighterCoreOptions } from "shiki";
-
-export const defaultMarkdownConfig = {
-	langs: ["javascript", "typescript"],
-	themes: ["vitesse-light"],
-} as unknown as HighlighterCoreOptions;
